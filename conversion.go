a9debb24a0b0181f514f6d3355a06b21ab89dad6
Bailey Matthews <baileyjmatthews@gmail.com>
2020-06-28 18:59:44 +0100

    New: Add wrappers for marshaling

---
+++ conversion.go
@@ -0,0 +1,33 @@
+package jexiasdkgo
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
+// marshal is an internal function wrapper for marshalling json payloads, may be more intricate in the future
+func marshal(payload interface{}) ([]byte, error) {
+	b, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Printf("error from marshal. response: %v", err)
+		return nil, err
+	}
+	return b, nil
+}
+
+// unmarshal is an internal function wrapper for unmarshalling json payloads from a ReadCloser, may be more intricate in the future
+func unmarshal(b []byte, target interface{}) error {
+	json.Unmarshal(b, &target)
+	return nil
+}
+
+// unmarshal is an internal function wrapper for unmarshalling json payloads from a ReadCloser, may be more intricate in the future
+func read(body io.Reader) ([]byte, error) {
+	b, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, err
+	}
+	return b, err
+}
---
+++ conversion_test.go
@@ -0,0 +1,72 @@
+package jexiasdkgo
+
+import (
+	"encoding/json"
+	"testing"
+	"io/ioutil"
+	"bytes"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarshal(t *testing.T) {
+	type test struct {
+		value string
+	}
+
+	input := test{
+		"testing",
+	}
+	
+	actualPure, err := json.Marshal(input)
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	actualMarshal, err := marshal(input)
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	assert.Equal(t, actualPure, actualMarshal)
+}
+
+func TestUnmarshal(t *testing.T) {
+	type test struct {
+		value string
+	}
+	var actualPure test
+	var actualMarshal test
+
+	input := []byte("value: string")
+	
+	err := json.Unmarshal(input, &actualPure)
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	err = unmarshal(input, &actualMarshal)
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	assert.Equal(t, actualPure, actualMarshal)
+}
+
+func TestRead(t *testing.T) {
+	// Generate two as the first call will empty the array for the second
+	bodyTarget := ioutil.NopCloser(bytes.NewReader([]byte("test string")))
+	bodyActual := ioutil.NopCloser(bytes.NewReader([]byte("test string")))
+
+	target, err := ioutil.ReadAll(bodyTarget)
+	if err != nil {
+		assert.Error(t, err)
+	}
+	
+	actual, err := read(bodyActual)
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	assert.Equal(t, target, actual)
+}
