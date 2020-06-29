a1c1e0e66b57201fa72bf1ec125e404d29711c91
Bailey Matthews <baileyjmatthews@gmail.com>
2020-06-29 18:46:32 +0100

    Test: Add both successful and failed response test

---
+++ error_test.go
@@ -0,0 +1,57 @@
+package jexiasdkgo
+
+import (
+	"bytes"
+	"net/http"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckErrorOnFailedCall(t *testing.T) {
+	var response *http.Response
+	var target APIError
+
+	target = APIError{
+		ID:      "some-really-long-id",
+		Message: "A really useful message",
+	}
+
+	body, err := marshal(target)
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	response = &http.Response{
+		StatusCode:    400,
+		Body:          ioutil.NopCloser(bytes.NewReader(body)),
+	}
+
+	err = checkError(response)
+	assert.Contains(t, err.Error(), target.ID)
+	assert.Contains(t, err.Error(), target.Message)
+}
+
+func TestCheckErrorOnSuccessfulCall(t *testing.T) {
+	var response *http.Response
+	var target APIError
+	
+	target = APIError{
+		ID:      "some-really-long-id",
+		Message: "A really useful message",
+	}
+
+	body, err := marshal(target)
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	response = &http.Response{
+		StatusCode:    200,
+		Body:          ioutil.NopCloser(bytes.NewReader(body)),
+	}
+
+	err = checkError(response)
+	assert.Equal(t, err, nil)
+}
