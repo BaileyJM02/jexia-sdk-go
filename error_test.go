2e2cc2e92b05bf414bb1a1bd201344402be29b03
Bailey Matthews <baileyjmatthews@gmail.com>
2020-07-08 22:30:26 +0100

    Test: Fix error test

--- error_test.go
+++ error_test.go
@@ -11,12 +11,12 @@ import (
 
 func TestCheckErrorOnFailedCall(t *testing.T) {
 	var response *http.Response
-	var target APIError
+	var target []APIError
 
-	target = APIError{
+	target = []APIError{APIError{
 		ID:      "some-really-long-id",
 		Message: "A really useful message",
-	}
+	}}
 
 	body, err := marshal(target)
 	if err != nil {
@@ -29,8 +29,8 @@ func TestCheckErrorOnFailedCall(t *testing.T) {
 	}
 
 	err = checkForAPIError(response)
-	assert.Contains(t, err.Error(), target.ID)
-	assert.Contains(t, err.Error(), target.Message)
+	assert.Contains(t, err.Error(), target[0].ID)
+	assert.Contains(t, err.Error(), target[0].Message)
 }
 
 func TestCheckErrorOnSuccessfulCall(t *testing.T) {
