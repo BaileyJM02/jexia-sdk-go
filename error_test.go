21c9d2c380dd1d0eecf32ed9a6273a9c067d2496
Bailey Matthews <baileyjmatthews@gmail.com>
2020-07-09 21:46:24 +0100

    Test: Add tests inline with new changes

--- error_test.go
+++ error_test.go
@@ -11,33 +11,28 @@ import (
 
 func TestCheckErrorOnFailedCall(t *testing.T) {
 	var response *http.Response
-	var target []APIError
+	var target []Error
 
-	target = []APIError{APIError{
+	target = []Error{{
 		ID:      "some-really-long-id",
 		Message: "A really useful message",
 	}}
 
-	body, err := marshal(target)
-	if err != nil {
-		assert.Error(t, err)
-	}
-
 	response = &http.Response{
 		StatusCode: 400,
-		Body:       ioutil.NopCloser(bytes.NewReader(body)),
+		Body:       ioutil.NopCloser(bytes.NewReader([]byte(`[{"request_id":"some-really-long-id","message":"A really useful message"}]`))),
 	}
 
-	err = checkForAPIError(response)
+	err := checkForAPIError(response)
 	assert.Contains(t, err.Error(), target[0].ID)
 	assert.Contains(t, err.Error(), target[0].Message)
 }
 
 func TestCheckErrorOnSuccessfulCall(t *testing.T) {
 	var response *http.Response
-	var target APIError
+	var target Error
 
-	target = APIError{
+	target = Error{
 		ID:      "some-really-long-id",
 		Message: "A really useful message",
 	}
