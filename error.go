607db1ce01b0ce26f5113394c61ab1f095616cf0
Bailey Matthews <baileyjmatthews@gmail.com>
2020-07-09 21:45:57 +0100

    Improve: New error type and params

--- error.go
+++ error.go
@@ -6,10 +6,26 @@ import (
 	"net/http"
 )
 
-// APIError is the error returned from a Jexia API endpoint
-type APIError struct {
-	ID      string `json:"request_id"`
-	Message string `json:"message"`
+// Internal signifies the error was send from an internal source
+const Internal = "Internal"
+
+// API signifies the error was send from the Jexia API directly
+const API = "API"
+
+// Error is the error returned, allows for retry scenarios
+type Error struct {
+	ID        string `json:"request_id"`
+	Message   string  `json:"message"`
+	Origin    string `json:"origin"`
+	Temporary bool   `json:"temporary"`
+}
+
+func (e *Error) Error() string {
+	temp := ""
+	if e.Temporary {
+		temp = "temporary"
+	}
+	return fmt.Sprintf("%v endpoint error: %v (Origin: %v) (ID: %v)", temp, e.Message, e.Origin, e.ID)
 }
 
 // checkForAPIError is an internal function wrapper for returning a more useful API error
@@ -18,11 +34,53 @@ func checkForAPIError(response *http.Response) error {
 	if response.StatusCode >= 200 && response.StatusCode < 300 {
 		return nil
 	}
-	var APIErr []APIError
+	var APIErr []Error
 	b, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return fmt.Errorf("Unable to read response body: %w", err)
+		return &Error{
+			ID:        "e001",
+			Message:   fmt.Errorf("Unable to read response body: %w", err).Error(),
+			Origin:    Internal,
+			Temporary: false,
+		}
 	}
-	unmarshal(b, &APIErr)
-	return fmt.Errorf("Endpoint error: %v (ID: %v)", APIErr[0].Message, APIErr[0].ID)
+	err = unmarshal(b, &APIErr)
+	if err != nil {
+		return &Error{
+			ID:        "e002",
+			Message:   fmt.Errorf("Unable to unmarshal body: %w", err).Error(),
+			Origin:    Internal,
+			Temporary: false,
+		}
+	}
+	// If there is a matching API error, return this as we can fail absolutely
+	if len(APIErr) > 0 {
+		return &Error{
+			ID:        APIErr[0].ID,
+			Message:   APIErr[0].Message,
+			Origin:    API,
+			Temporary: false,
+		}
+	}
+	// Such unknown error may allow us to retry, it could be due to a network connection drop etc.
+	return &Error{
+		ID:        "e003",
+		Message:   fmt.Errorf("Unknown error, error does not match predefined parameters, presumed failed call").Error(),
+		Origin:    Internal,
+		Temporary: true,
+	}
+}
+
+func getNiceError(err error, message string) *Error {
+	switch err.(type) {
+	case *Error:
+		return err.(*Error)
+	default:
+		return &Error{
+			ID:        "e004",
+			Message:   fmt.Errorf("%v: %v\n", message, err).Error(),
+			Origin:    Internal,
+			Temporary: false,
+		}
+	}
 }
